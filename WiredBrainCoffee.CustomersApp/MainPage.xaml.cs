2bf52ccab62f8994d37c283715b148cae41dda40
Kamlesh Kishor <imkamlesh.25@gmail.com>
2022-01-17 00:40:42 +0530

    Add project files.

---
+++ WiredBrainCoffee.CustomersApp/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using WiredBrainCoffee.CustomersApp.DataProvider;
+using WiredBrainCoffee.CustomersApp.Model;
+
+namespace WiredBrainCoffee.CustomersApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private CustomerDataProvider _customerDataProvider;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+            App.Current.Suspending += App_Suspending;
+            _customerDataProvider = new CustomerDataProvider();
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            customerListView.Items.Clear();
+            var customers = await _customerDataProvider.LoadCustomersAsync();
+            foreach (var customer in customers)
+            {
+                customerListView.Items.Add(customer);
+            }
+        }
+
+        private async void App_Suspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            await _customerDataProvider.SaveCustomerAsync(
+                customerListView.Items.OfType<Customer>());
+            deferral.Complete();
+        }
+
+        private void ButtonAddCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            var customer = new Customer { FirstName = "New" };
+            customerListView.Items.Add(customer);
+            customerListView.SelectedItem = customer;
+        }
+
+        private void ButtonDeleteCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            var customer = customerListView.SelectedItem as Customer;
+            if (customer!=null)
+                customerListView.Items.Remove(customer);
+        } 
+
+        private void ButtonMove_Click(object sender, RoutedEventArgs e)
+        {
+            //int column = (int)customerListGrid.GetValue(Grid.ColumnProperty);
+            int column = Grid.GetColumn(customerListGrid);
+            int newColumn = column == 0 ? 2 : 0;
+            //customerListGrid.SetValue(Grid.ColumnProperty, newColumn);
+            Grid.SetColumn(customerListGrid, newColumn);
+            moveSymbolIcon.Symbol = newColumn == 0 ? Symbol.Forward : Symbol.Back;
+        }
+
+        private void customerListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var customer = customerListView.SelectedItem as Customer;
+            customerDetailControl.Customer = customer;
+        }
+    }
+}
---
+++ WiredBrainCoffee.CustomersApp/Model/Customer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WiredBrainCoffee.CustomersApp.Base;
+
+namespace WiredBrainCoffee.CustomersApp.Model
+{
+    public class Customer : Observable
+    {
+        private string _firstName;
+        private string _lastName;
+        private bool _isDeveloper;
+
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged();
+            }
+        }
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool IsDeveloper
+        {
+            get => _isDeveloper;
+            set
+            {
+                _isDeveloper = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
